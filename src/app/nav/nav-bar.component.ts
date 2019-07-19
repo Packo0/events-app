a2f385e114356b126e0647ab807a76e1cf6833d7
Dimitrov <Pavel.Dimitrov@experian.com>
2019-07-19 12:27:55 +0300

    git sync

--- src/app/nav/nav-bar.component.ts
+++ src/app/nav/nav-bar.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styles: [`
-    .nav.navbar-nav {font-size: 15px}
-    #searchForm {margin-right: 100px}
-    @media (max-width: 1200px) {#searchForm{display: none}}
+    .nav.navbar-nav {font-size: 15px;}
+    #searchForm {margin-right: 100px;}
+    @media (max-width: 1200px) {#searchForm {display:none}}
   `]
 })
 export class NavBarComponent {
+
 }
