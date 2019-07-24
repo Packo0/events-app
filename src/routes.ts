3c3cdb26030c98ff77ac9dc36ccc0370fd903536
Dimitrov <Pavel.Dimitrov@experian.com>
2019-07-24 10:27:56 +0300

    add router guard as a service

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { EventsListComponent } from './app/events/events-list.component';
 import { EventDetailsComponent } from './app/events/event-details/event-details.component';
 import { CreateEventComponent } from './app/events/create-event.component';
+import { EventRouteActivator as EventRouteActivator } from './app/events/event-details/event-route-activator.service';
+import { Error404Component } from './app/error/404.component';
 
 export const appRoutes: Routes = [
   { path: 'events', component: EventsListComponent },
   { path: 'events/new', component: CreateEventComponent },
-  { path: 'events/:id', component: EventDetailsComponent },
+  { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+  { path: '404', component: Error404Component },
   { path: '**', redirectTo: 'events', pathMatch: 'full' },
 ];
