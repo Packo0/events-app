17c8dbcc8b2e5381334696290cab018515578df2
packo0 <pdimitrov1389@gmail.com>
2019-07-27 12:56:27 +0300

    adding auth service with fake login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavBarComponent } from './nav/nav-bar.component'
 import { ToastrService } from './common/toastr.service'
 import { appRoutes } from './../routes';
 import { Error404Component } from './error/404.component';
+import { AuthService } from './user/auth.service';
 
 @NgModule({
   imports: [
@@ -27,6 +28,7 @@ import { Error404Component } from './error/404.component';
     ToastrService,
     EventRouteActivator,
     EventListResolver,
+    AuthService,
     {
       provide: 'canDeactivateCreateEvent',
       useValue: checkDirtyState
